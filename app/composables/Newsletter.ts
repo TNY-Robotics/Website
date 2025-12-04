eb60acdd204908514fd17525c68dd6c097abd000
FurWaz <fur.waz06@gmail.com>
2025-12-04 13:40:45 +0100

    Added url option for newsletter

--- app/composables/Newsletter.ts
+++ app/composables/Newsletter.ts
@@ -1,4 +1,24 @@
 const modalOpenModel = ref(false);
+
 export const useNewsletter = () => {
+  const router = useRouter();
+  const route = useRoute();
+
+  watch(modalOpenModel, (newVal) => {
+    if (newVal) {
+      const query = { ...route.query, newsletter: 'true' };
+      router.replace({ query });
+    }
+    else {
+      const query = { ...route.query };
+      delete query.newsletter;
+      router.replace({ query });
+    }
+  });
+
+  watch(() => route.query.newsletter, (newVal) => {
+    modalOpenModel.value = newVal === 'true';
+  }, { immediate: true });
+
   return { modalOpenModel, openModal: () => modalOpenModel.value = true };
 }
