cce9281e0979c9f4a5b7f163afbb6b251a707cdc
FurWaz <fur.waz06@gmail.com>
2025-12-13 18:48:10 +0100

    Fixed mobile view

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="flex flex-col grow pt-24 px-4 space-y-32">
-        <div class="flex w-full h-fit justify-evenly items-center pt-32">
-            <div class="flex p-8 bg-white dark:bg-slate-900 rounded-lg shadow-lg max-w-7xl">
-                <div class="space-y-4 w-3/5">
-                    <h2 class="text-4xl font-bold pb-8"> <RichText path="about.mission.title" /> </h2>
-                    <p class="text-xl"> <RichText path="about.mission.description1" /> </p>
-                    <p class="text-xl"> <RichText path="about.mission.description2" /> </p>
+    <div class="flex flex-col grow pt-24 px-2 lg:px-4 space-y-32">
+        <div class="flex w-full h-fit justify-evenly items-center pt-16 lg:pt-32">
+            <div class="flex flex-col lg:flex-row p-4 lg:p-8 bg-white dark:bg-slate-900 rounded-lg shadow-lg lg:max-w-7xl">
+                <div class="space-y-4 lg:w-3/5">
+                    <h2 class="text-3xl lg:text-4xl font-bold pb-8"> <RichText path="about.mission.title" /> </h2>
+                    <p class="text-lg lg:text-xl"> <RichText path="about.mission.description1" /> </p>
+                    <p class="text-lg lg:text-xl"> <RichText path="about.mission.description2" /> </p>
                 </div>
-                <div class="flex justify-center items-center w-2/5">
+                <div class="flex justify-center items-center lg:w-2/5 py-8 lg:py-0">
                     <TNYLogo class="w-32 lg:w-48 z-20 drop-shadow-lg drop-shadow-slate-800/30" />
                 </div>
             </div>
         </div>
-        <div class="flex flex-col w-full h-fit justify-evenly items-center pt-32 space-y-8">
+        <div class="flex flex-col w-full h-fit justify-evenly items-center pt-16 lg:pt-32 space-y-8">
             <div class="flex flex-col space-y-4 justify-center items-center ">
-                <h2 class="text-4xl font-bold"> <RichText path="about.dna.title" /> </h2>
-                <p class="text-xl"> <RichText path="about.dna.description" /> </p>
+                <h2 class="text-3xl lg:text-4xl font-bold"> <RichText path="about.dna.title" /> </h2>
+                <p class="text-lg lg:text-xl"> <RichText path="about.dna.description" /> </p>
             </div>
-            <div class="flex justify-evenly items-center h-fit">
+            <div class="flex flex-col lg:flex-row justify-evenly items-center h-fit">
                 <div class="flex flex-col border-2 border-slate-200 dark:border-slate-700 shadow-lg rounded m-4 p-4 max-w-sm h-48 space-y-4">
                     <h3 class="text-2xl font-semibold text-center text-primary"><RichText path="about.dna.lego.title" /></h3>
                     <p class="flex grow justify-center items-center text-center"><RichText path="about.dna.lego.content" /></p>
@@ -32,9 +32,9 @@
                 </div>
             </div>
         </div>
-        <div class="flex w-full justify-evenly items-center pt-32 pb-32">
+        <div class="flex flex-col lg:flex-row space-y-12 lg:space-y-0 w-full justify-evenly items-center pt-16 lg:pt-32 pb-32">
             <div class="flex flex-col w-fit space-y-8 max-w-3xl">
-                <h2 class="text-4xl font-bold"> <RichText path="about.founder.title" /> </h2>
+                <h2 class="text-3xl lg:text-4xl font-bold"> <RichText path="about.founder.title" /> </h2>
                 <p class="text-xl"> <RichText path="about.founder.description" /> </p>
             </div>
             <div class="flex w-fit">
